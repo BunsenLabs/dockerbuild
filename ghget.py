137d62e57e5f1f79ce3a1f3ce643f2f8e3230ef3
Jens John <dev@2ion.de>
2019-12-30 12:18:27 +0100

    deploy: set +x flag

--- ghget.py
+++ ghget.py
@@ -1,15 +1,42 @@
 #!/usr/bin/env python3
 from argparse import ArgumentParser, Namespace
+from dataclasses import dataclass, field
 from debian.debian_support import Version, version_compare
 from dockerbuild.build import build
 from functools import cmp_to_key
 from github import Github
 from pathlib import Path
+from tarfile import TarFile
+from tempfile import TemporaryDirectory
 from typing import Dict, List, Optional
 import os
 import requests
 import shutil
 import sys
+
+@dataclass
+class Buildjob:
+    project: str
+    tag: str
+    architectures: List[str]
+    tarball_url: str = field(default_factory=str)
+
+    def resolve(self, github: Github) -> object:
+        repo = github.get_repo(self.project)
+        tags = repo.get_tags()
+        if tags.totalCount == 0:
+            raise ValueError(f'<{self.project}> has no tags')
+        if self.tag == '?':
+            versions = sorted(map(lambda t: Version(t.name), tags), key=cmp_to_key(version_compare))
+            self.tag = latest = str(versions[-1])
+            self.tarball_url = list(filter(lambda t: t.name == latest, tags))[0].tarball_url
+            return self
+        else:
+            for t in tags:
+                if self.tag == t.name:
+                    self.tarball_url = t.tarball_url
+            return self
+        raise KeyError(f'could not resolve or validate tag for project: {self.project}:{self.tag}')
 
 def namespace(**kwargs) -> Namespace:
     ns = Namespace()
@@ -17,9 +44,15 @@ def namespace(**kwargs) -> Namespace:
         setattr(ns, k, v)
     return ns
 
+def checktar(tarball: TarFile) -> None:
+    blacklist = [ '/', '..' ]
+    for m in tarball.getmembers():
+        assert not (m.name[0] in blacklist)
+
 def getopts() -> Namespace:
     ap = ArgumentParser()
     ap.add_argument("-o", "--output-dir", type=Path, default=Path(os.getcwd()))
+    ap.add_argument("-b", "--build-dir", type=Path, default=Path(os.getcwd()))
     ap.add_argument("project_list", nargs='+', default=[])
     return ap.parse_args()
 
@@ -31,40 +64,29 @@ def getenv(ns: Namespace, *args) -> Namespace:
         setattr(ns, env, v)
     return ns
 
-def resolve_project_tags(github: Github, projects: Dict[str,str]) -> Dict[str,str]:
-    def validate_tag(project: str, tag: str) -> Optional[str]:
-        repo = github.get_repo(project)
-        tags = repo.get_tags()
-        if tags.totalCount == 0:
-            raise ValueError(f'<{project}> has no tags')
-        if tag == '?':
-            versions = sorted(map(lambda t: Version(t.name), tags), key=cmp_to_key(version_compare))
-            latest = str(versions[-1])
-            return list(filter(lambda t: t.name == latest, tags))[0].tarball_url
-        else:
-            for t in tags:
-                if tag == t.name:
-                    return t.tarball_url
-        raise KeyError(f'could not resolve or validate tag for project: {project}:{tag}')
-    return { project: validate_tag(project, tag) for project, tag in projects.items() }
-
 def main() -> int:
     opts = getopts()
     opts = getenv(opts, 'GITHUB_API_TOKEN')
     github = Github(opts.GITHUB_API_TOKEN)
-    projects = {}
+    projects = []
 
     print('Parsing project list...')
+    # <project>:<tag>:<architectures>
     for p in opts.project_list:
-        if '=' in p:
-            project, tag = p.split('=', 1)
+        delim = p.count(':')
+        if delim == 2:
+            project, tag, architectures = p.split(':', 2)
+            architectures = architectures.split(',')
+        elif delim == 1:
+            project, tag = p.split(':', 1)
+            architectures = ['amd64']
         else:
             project = p
             tag = '?'
-        projects[project] = tag
+            architectures = ['amd64']
+        projects.append(Buildjob(project=project,
+            tag=tag, architectures=architectures).resolve(github))
 
-    print('Resolving project tags on github.com...')
-    projects = resolve_project_tags(github, projects)
 
     print('Downloading tarballs...')
     local_jobs = {}
@@ -81,6 +103,14 @@ def main() -> int:
             shutil.copyfileobj(req.raw, FILE)
         print(f'DOWNLOAD OK: {project} => {output_path}')
         local_jobs[project] = output_path
+
+
+    print('Building projects...')
+    for project, tarball_path in local_jobs.items():
+        with TemporaryDirectory() as DIR:
+            with TarFile(tarball_path) as TAR:
+                checktar(TAR)
+                TAR.extractall(path=DIR)
 
     return 0
 
