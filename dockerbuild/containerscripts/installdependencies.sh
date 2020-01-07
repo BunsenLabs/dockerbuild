0810dc07b60fea40e8a03a37fae165059ed580bb
Jens John <dev@2ion.de>
2020-01-07 21:13:57 +0100

    containerscripts: git not part of devscripts in deb8

--- dockerbuild/containerscripts/installdependencies.sh
+++ dockerbuild/containerscripts/installdependencies.sh
@@ -4,7 +4,7 @@ SCRIPTDIR=$(readlink -f "$(dirname "$0")")
 
 source "$SCRIPTDIR"/common.sh
 
-xinstall devscripts
+xinstall devscripts git
 
 yes | mk-build-deps -i "$PKGDIR"/debian/control
 rm -- *build-deps*
