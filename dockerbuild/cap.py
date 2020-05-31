93d3d64f6c01fb595a97ccb432b9dffb4e68c81b
Jens John <dev@2ion.de>
2020-05-31 19:35:45 +0200

    cap: Simple library for getting a PID's set of capabilities
    This will be useful when determining whether we can setsid()/setgid()
    the download agent process even if we are not root.

---
+++ dockerbuild/cap.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+
+""" Enumerate process capabilities from procfs, on Linux """
+
+from pathlib import Path
+from typing import Tuple, Set
+
+PROCFS = Path("/proc")
+
+CAPABILITIES = {
+    0  : 'CAP_CHOWN',
+    1  : 'CAP_DAC_OVERRIDE',
+    2  : 'CAP_DAC_READ_SEARCH',
+    3  : 'CAP_FOWNER',
+    4  : 'CAP_FSETID',
+    5  : 'CAP_KILL',
+    6  : 'CAP_SETGID',
+    7  : 'CAP_SETUID',
+    8  : 'CAP_SETPCAP',
+    9  : 'CAP_LINUX_IMMUTABLE',
+    10 : 'CAP_NET_BIND_SERVICE',
+    11 : 'CAP_NET_BROADCAST',
+    12 : 'CAP_NET_ADMIN',
+    13 : 'CAP_NET_RAW',
+    14 : 'CAP_IPC_LOCK',
+    15 : 'CAP_IPC_OWNER',
+    16 : 'CAP_SYS_MODULE',
+    17 : 'CAP_SYS_RAWIO',
+    18 : 'CAP_SYS_CHROOT',
+    19 : 'CAP_SYS_PTRACE',
+    20 : 'CAP_SYS_PACCT',
+    21 : 'CAP_SYS_ADMIN',
+    22 : 'CAP_SYS_BOOT',
+    23 : 'CAP_SYS_NICE',
+    24 : 'CAP_SYS_RESOURCE',
+    25 : 'CAP_SYS_TIME',
+    26 : 'CAP_SYS_TTY_CONFIG',
+    27 : 'CAP_MKNOD',
+    28 : 'CAP_LEASE',
+    29 : 'CAP_AUDIT_WRITE',
+    30 : 'CAP_AUDIT_CONTROL',
+    31 : 'CAP_SETFCAP',
+    32 : 'CAP_MAC_OVERRIDE',
+    33 : 'CAP_MAC_ADMIN',
+    34 : 'CAP_SYSLOG',
+    35 : 'CAP_WAKE_ALARM',
+    36 : 'CAP_BLOCK_SUSPEND',
+    37 : 'CAP_AUDIT_READ',
+}
+
+FULLCAP = 0x3FFFFFFFFF
+
+def get_process_capabilities(pid: int) -> Tuple[Set[str], Set[int]]:
+    """ On Linux, return a set of all the capabilities the given PID has. The second return tuple
+    item is a set of the hex values of all unknown capability values found. This set should be empty
+    if this function is implemented correctly for the given kernel. As the list of caps may be
+    expanded in the future, on some kernels, the second set may not be empty. """
+    if not PROCFS.exists():
+        raise NotImplementedError("procfs not found at /proc -- require procfs at that location")
+    status = Path(f"/proc/{pid}/status")
+
+    effective_caps = set()
+    unknown_effective_caps = set()
+
+    with status.open("r") as FILE:
+        for line in FILE:
+            if line.startswith("CapEff"):
+                fields = line.split(" ", 1)
+                caps = int(line[:-1].split(chr(0x09), 1)[-1], 16)
+                if caps == FULLCAP:
+                    return set(CAPABILITIES.values()), unknown_effective_caps
+                for i in range(0, 64, 1):
+                    if ((caps >> i) & 0x1) == 1:
+                        if capstr := CAPABILITIES.get(i, None):
+                            effective_caps.add(capstr)
+                        else:
+                            unknown_effective_caps.add(i)
+    return effective_caps, unknown_effective_caps
