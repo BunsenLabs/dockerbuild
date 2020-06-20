ff8492f9c043dc7c3bdc5d6f1d5c641adf4a928c
Jens John <dev@2ion.de>
2020-06-20 19:09:49 +0200

    download_agent: implement proper setuid/setgid and test

--- dockerbuild/download_agent.py
+++ dockerbuild/download_agent.py
@@ -20,8 +20,20 @@ class DownloadAgent:
         parent = dest.parent
         _setgid = lambda: True
         _setuid = lambda: True
-        can_setgid = has_caps(os.getpid(), set([ 'CAP_SETGID' ]))
-        can_setuid = has_caps(os.getpid(), set([ 'CAP_SETUID' ]))
+
+        if has_caps(os.getpid(), set([ 'CAP_SETGID' ])):
+            logger.debug("Have CAP_SETGID, preparing privdrop for GID")
+            _setgid = lambda: os.setgid(self.__opts.unpriv_gid)
+            shutil.chown(dest, group=self.__opts.unpriv_uid)
+            dest.chmod(dest.stat().st_mode | 0o00070)
+            logger.debug("Changed mode of <%s> to %o for setgid download", dest, dest.stat().st_mode)
+
+        if has_caps(os.getpid(), set([ 'CAP_SETUID' ])):
+            logger.debug("Have CAP_SETUID, preparing privdrop for UID")
+            _setuid = lambda: os.setuid(self.__opts.unpriv_uid)
+            shutil.chown(dest, owner=self.__opts.unpriv_uid)
+            dest.chmod(dest.stat().st_mode | 0o00700)
+            logger.debug("Changed mode of <%s> to %o for setuid download", dest, dest.stat().st_mode)
 
         if dest.exists():
             logger.error('Download destination already exists, refusing to overwrite')
@@ -31,17 +43,10 @@ class DownloadAgent:
             logger.error('Download destination parent is not a directory: %s', dedst)
             return False
 
-        p = Process(target=self.__get, args=(
-            _setgid,
-            _setuid,
-            self.__opts.unpriv_uid,
-            self.__opts.unpriv_gid,
-            url,
-            dest,)
-        )
+        p = Process(target=self.__get, args=(_setgid, _setuid, url, dest,))
         p.start()
         p.join()
-        logger.error("%s %d", str(p), p.exitcode)
+
         if p.exitcode != 0:
             logger.error('Download process exited with status %d', p.exitcode)
             return False
@@ -53,12 +58,16 @@ class DownloadAgent:
         return True
 
     @staticmethod
-    def __get(setgid: Callable, setuid: Callable, uid: int, gid: int, url: str, path: Path) -> int:
-        if not (setgid() and setuid()):
-            raise OSError(f"Failed to drop privileges")
+    def __get(setgid: Callable, setuid: Callable, url: str, path: Path) -> int:
+        assert setgid(), "Failed to call setgid() callable"
+        assert setuid(), "Failed to call setuid() callable"
         with path.open("wb") as FILE:
+            logger.debug("Opened file for writing: %s", path)
             resp = requests.get(url, stream=True, allow_redirects=True)
+            logger.debug("Opened request: %s", url)
             resp.raw.decode_content = True # gunzip
+            logger.debug("Writing download...")
             shutil.copyfileobj(resp.raw, FILE)
+            logger.debug("Finished writing download")
             resp.close()
         return 0
--- tests/test_download_agent.py
+++ tests/test_download_agent.py
@@ -1,9 +1,11 @@
 from dockerbuild.download_agent import DownloadAgent
 
 def test_canary_download(tmp_path, opts_nobody_uid_gid):
+        gzip_header = bytes((0x1f, 0x8b))
         agent = DownloadAgent(opts_nobody_uid_gid)
-        tmp_path.chmod(0o777)
         dest = tmp_path / "destination.tar.gz"
         result = agent.download("https://github.com/BunsenLabs/bunsen-os-release/archive/10.4.0-1.tar.gz", dest)
         assert result == True
-        #return result == True and dest.is_file() and dest.stat().st_size != 0
+        assert dest.is_file()
+        assert dest.stat().st_size != 0
+        assert dest.open("rb").read()[0:2] == gzip_header
